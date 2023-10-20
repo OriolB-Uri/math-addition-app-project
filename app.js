14c05e354ab15c9052c0af658a35751dcf243d53
OriolB-Uri <oriolbotella@hotmail.com>
2023-10-20 13:35:09 +0200

    hecho

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-//generate random numbers. Utilizo la biblioteca Chance !
-// https://chancejs.com/basics/integer.html
-
-let firstNumber = chance.integer({ min: 0, max: 10 })
-
-let secondNumber = chance.integer({ min: 0, max: 10 })
-
-//get the total
+let firstNumber = Math.floor(Math.random() * 11);
+let secondNumber = Math.floor(Math.random() * 11);
 let total = firstNumber + secondNumber;
+
+let primaryNumber = document.querySelector("#primary-number"); 
+let secondaryNumber = document.querySelector("#secondary-number");
+let input = document.querySelector("#guess");
+let message = document.querySelector("#message");
+let button = document.querySelector("#btn");
+
+primaryNumber.textContent = firstNumber;
+secondaryNumber.textContent = secondNumber;
+
+function checkResult(){
+    if (input.value == total){
+        message.textContent = "¡CORRECTO! El resultado es " + total;
+        message.style.color = "green";
+    } else {
+        message.textContent = "¡INCORRECTO! El resultado es " + total;
+        message.style.color = "red";
+    }
+}
+
+button.addEventListener("click", checkResult);
